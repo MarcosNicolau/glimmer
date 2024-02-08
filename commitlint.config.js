42e947d84b7052e3a3fd397f93a96a9354dd623a
nicolau <marcosnicolau2003@gmail.com>
2024-02-08 14:30:38 -0300

    chore(global): 🔧 setup commitlint, commitizen and husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,29 @@
-module.exports = {
+const config = {
 	extends: ["@commitlint/config-conventional"],
 	//Ignore bump messages for dependabot bump commits, sometimes creates headers longer than the character limit thus failing the workflow
 	ignores: [(message) => message.includes("chore(release)")],
+	rules: {
+		"scope-enum": [
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			require("@commitlint/types").RuleConfigSeverity.Warning,
+			"always",
+			// List all the repo packages here
+			["global", "gogol"],
+		],
+	},
+	prompt: {
+		questions: {
+			scope: {
+				description: "Select the project",
+				enum: {
+					gogol: {
+						description: "Voice server",
+						title: "Gogol",
+					},
+				},
+			},
+		},
+	},
 };
+
+module.exports = config;
